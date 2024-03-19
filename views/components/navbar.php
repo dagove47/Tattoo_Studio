4e236ae35c1cb0dd38ee0b83b54e18d816a7037a
David Gómez <dagove47@gmail.com>
2024-03-18 20:24:39 -0600

    Navbar and footer added and first homepage section

---
+++ views/components/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Nebularte</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+        <div class="me-auto"></div>
+        <ul class="navbar-nav">
+            <li class="nav-item d-flex justify-content-center">
+            <a class="nav-link px-3" href="#home">Inicio</a>
+            </li>
+            <li class="nav-item d-flex justify-content-center">
+            <a class="nav-link px-3" href="#about">Nosotros</a>
+            </li>
+            <li class="nav-item d-flex justify-content-center">
+            <a class="nav-link px-3" href="#work">Precio</a>
+            </li>
+            <li class="nav-item d-flex justify-content-center">
+            <a class="nav-link px-3" href="#contact">Contacto</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
