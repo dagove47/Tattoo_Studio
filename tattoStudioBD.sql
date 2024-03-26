726b68d85b6b70d5cf3e97e1fba002ae9b618466
David Gómez <dagove47@gmail.com>
2024-03-25 21:10:07 -0600

    BD update

---
+++ tattoStudioBD.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE studio_tatto;
+
+-- Seleccionar la base de datos
+USE Studio_tatto;
+
+-- Crear la tabla Clientes
+CREATE TABLE Clientes (
+    ClienteID INT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    Apellido VARCHAR(255),
+    CorreoElectronico VARCHAR(255),
+    Contraseña VARCHAR(255),
+    FechaNacimiento DATE
+);
+
+-- Crear la tabla Tatuajes
+CREATE TABLE Tatuajes (
+    TatuajeID INT PRIMARY KEY,
+    NombreTatuaje VARCHAR(255),
+    Categoria VARCHAR(255),
+    ZonaCuerpo VARCHAR(255),
+    Tecnica VARCHAR(255),
+    DiseñoURL VARCHAR(255)
+);
+
+-- Crear la tabla Transacciones
+CREATE TABLE Transacciones (
+    TransaccionID INT PRIMARY KEY,
+    ClienteID INT,
+    TatuajeID INT,
+    MontoPagado DECIMAL(10, 2),
+    FechaTransaccion DATE,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ClienteID),
+    FOREIGN KEY (TatuajeID) REFERENCES Tatuajes(TatuajeID)
+);
+
+CREATE TABLE Cotizaciones (
+Cotizacion_ID INT PRIMARY KEY,
+Cliente_ID INT,
+Diseño VARCHAR(250),
+DETALLE_COTIZACION VARCHAR(250),
+FOREIGN KEY (Cliente_ID) REFERENCES Clientes(ClienteID)
+);
+
+CREATE TABLE EVENTO_AGENDA(
+Evento_ID INT PRIMARY KEY ,
+Cliente_ID int,
+Descripcion varchar(230),
+Foreign key (Cliente_ID) references Clientes(ClienteID)
+);
+
+CREATE TABLE ADMINISTRADORES(
+AdminID INT PRIMARY KEY,
+Nombre_Usuario Varchar(15),
+Contra varchar(10)
+);
