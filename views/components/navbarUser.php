21b20a800c91ac5d2c4c5d6cfdc5ee95f8c2b810
davidgomez-sa <dagove47@gmail.com>
2024-04-22 00:04:42 -0600

    last update

--- views/components/navbarUser.php
+++ views/components/navbarUser.php
@@ -27,7 +27,7 @@
             <a class="nav-link px-3" href="./contact.php">Contacto</a>
             </li>
             <li class="nav-item d-flex justify-content-center">
-            <a class="nav-link px-3" href="./pages/login.php">Salir</a>
+            <a class="nav-link px-3" href="./login.php">Salir</a>
             </li>
         </ul>
         </div>
